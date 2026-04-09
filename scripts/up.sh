78ecd3557d3cacdf97d1ab3f2afce964c6ee6893
Danny Vaca <danny270793@icloud.com>
2026-04-09 13:35:25 -0500

    Enhance up.sh script to support environment-specific Docker Compose commands. Added options for local, dev, and prod environments, allowing for flexible configuration based on user input.

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,5 +1,35 @@
 set -e
-
 cd "$(dirname "$0")/.."
 
-docker compose --env-file .env --env-file .env.local -f compose.yml -f compose.local.yml up
+flavour=dev
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --local)
+      flavour=local
+      shift
+      ;;
+    --dev)
+      flavour=dev
+      shift
+      ;;
+    --prod)
+      flavour=prod
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+case "$flavour" in
+  local)
+    docker compose --env-file .env --env-file .env.local -f compose.yml -f compose.local.yml up --build "$@"
+    ;;
+  dev)
+    docker compose --env-file .env --env-file .env.dev -f compose.yml -f compose.dev.yml up "$@"
+    ;;
+  prod)
+    docker compose --env-file .env --env-file .env.prod -f compose.yml -f compose.prod.yml up "$@"
+    ;;
+esac
